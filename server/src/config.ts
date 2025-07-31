63de32a4f7ebb3b921b50db9b56ad7143a79cb72
vipin2510 <77065205+vipin2510@users.noreply.github.com>
2025-07-31 17:11:57 +0530

    Update config.ts
    added more mail

--- server/src/config.ts
+++ server/src/config.ts
@@ -13,7 +13,8 @@ export const AUTH_CONFIG = {
         "rngautambablu@gmail.com","krishnaksahu05@gmail.com","grogarey29@gmail.com","sureshbhilaicg@gmail.com","riteshyadav.its@gmail.com",
         "mukeshpatel7977@gmail.com","prathamsahu2809@gmail.com","krrish9755@gmail.com","kuldeepma93@gmail.com","painkraamrita007@gmail.com","kuldeepmama93@gmail.com","opscellkanker@gmail.com"
         ,"apsabadra@gmail.com","apsabadra@gmail.com","sudsrkrace@gmail.com","naxalops@gmail.com","nksicg78@gmail.com",'Jitendrairs@gmail.com',
-'mayankgurjar108@gmail.com',
+'mayankgurjar108@gmail.com',"kumarravindrasopiya@gmail.com", "mayankransingh@gmail.com", "brijkishor425@gmail.com", "navinekka21@gmail.com", "Tpyadav600@gmail.com", "vinitsahu3600@gmail.com", "roshanahuja88@gmail.com", "chandrhass1994@gmail.com", "amankumarisjha@gmail.com",
+        "ghanshyamkamde2014@gmail.com", "amankumarji421@gmail.com", "dspckgoverna@gmail.com",
         // Add more authorized email addresses here
     ],
     GOOGLE_CLIENT_ID: "20470304601-qheou005agkgg61ab2a36amrkbdm846t.apps.googleusercontent.com"
